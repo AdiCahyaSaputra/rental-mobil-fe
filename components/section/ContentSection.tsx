8fb1c319b57f941ca2ad01ff0c6b183e8c1b9bd8
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2022-09-10 15:15:51 +0700

    footer

--- components/section/ContentSection.tsx
+++ components/section/ContentSection.tsx
@@ -12,7 +12,7 @@ const fakeData: ProductItemInterface[] = [
 
   { name: 'Toyota', modelYear: '2022', color: 'Merah', owner: 'John Doe' },
   { name: 'Daihatsu', modelYear: '1996', color: 'Hijau', owner: 'Mark Hill' },
-  { name: 'Jeep', modelYear: '2013', color: 'Biru', owner: 'Amanda Rose' },
+  { name: 'Jeep', modelYear: '2013', color: 'Biru', owner: 'Rose Notch' },
   { name: 'Mustibisa', modelYear: '2006', color: 'Kuning', owner: 'Jamal Eron' },
   { name: 'Hyundai', modelYear: '1987', color: 'Hitam', owner: 'George Ben' },
   { name: 'Isuzu', modelYear: '1999', color: 'Pink', owner: 'Natalie Fang' },
@@ -39,7 +39,7 @@ const ContentSection: React.FC = () => {
               placeholder="Cari merek mobil"
             />
             <button className="p-1.5 bg-white shadow-md">
-              <SearchIcon className='w-6 p-1 aspect-square'/>
+              <SearchIcon className='w-6 p-1 aspect-square' />
             </button>
 
           </div>
@@ -50,12 +50,12 @@ const ContentSection: React.FC = () => {
       <Container>
         <div className="grid grid-cols-12 gap-4 pb-20">
           {fakeData.map((data, index) => (
-            <ProductCard 
-              key={index} 
-              name={data.name} 
-              modelYear={data.modelYear} 
-              color={data.color} 
-              owner={data.owner} 
+            <ProductCard
+              key={index}
+              name={data.name}
+              modelYear={data.modelYear}
+              color={data.color}
+              owner={data.owner}
             />
           ))}
         </div>
