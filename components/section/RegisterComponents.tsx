b58d93cfcf86bc571100e05e6d293cd3c5f86bb1
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2022-09-17 18:10:05 +0700

    validation changes

--- components/section/RegisterComponents.tsx
+++ components/section/RegisterComponents.tsx
@@ -1,10 +1,13 @@
 // Lib
 import { useState } from "react"
 import { Formik, Field } from 'formik'
-import { validateForm } from "lib/utils/validator"
+import { validateRegisterForm } from "lib/utils/validation"
 
 // Components
 import RoleCard from "components/reusable/RoleCard"
+
+// Interface 
+import UserData from 'lib/interface/UserData'
 
 // Icons
 import EditIcon from '../../asset/svg/edit.svg'
@@ -33,7 +36,9 @@ const initFormValue = {
   role_id: 1
 }
 
-const submitHandler = (values: any, { setSubmitting }: any) => {
+const submitHandler = (values: UserData, { setSubmitting }: any) => {
+  const { name, email, address, mobile_phone, password, password_confirmation, role_id } = values
+
   setSubmitting(true)
   console.log(values)
 }
@@ -41,19 +46,15 @@ const submitHandler = (values: any, { setSubmitting }: any) => {
 const RegisterComponents: React.FC = () => {
 
   const [openModalRole, setOpenModalRole] = useState(false)
-  const [role, setRole] = useState({
-    id: 1,
-    name: 'CUSTOMER'
-  })
+  const [role, setRole] = useState({ id: 1, name: 'CUSTOMER' })
 
   return (
     <>
-      <Formik validate={validateForm} initialValues={initFormValue} onSubmit={submitHandler}>
+      <Formik validate={validateRegisterForm} initialValues={initFormValue} onSubmit={submitHandler}>
 
-        {({ values, errors, isSubmitting, handleSubmit }: any) => (
+        {({ values, errors, isSubmitting, handleSubmit, touched }: any) => (
           <>
             <pre className="overflow-auto">{JSON.stringify(values, null, 2)}</pre>
-            <pre className="overflow-auto">{JSON.stringify(errors, null, 2)}</pre>
 
             <form className='mt-6' onSubmit={e => {
               e.preventDefault()
@@ -63,21 +64,27 @@ const RegisterComponents: React.FC = () => {
               <div className='space-y-4'>
 
                 {inputElements.map(({ type, name, placeholder }, index: number) => (
-                  <Field
-                    key={index}
-                    placeholder={placeholder}
-                    autocomplete="off"
-                    type={type}
-                    name={name}
-                    value={values[name]}
-                    className="border-b-2 border-white/20 w-full outline-none bg-black text-white focus:border-b-2 focus:border-white p-1.5"
-                  />
+                  <div key={index}>
+                    <Field
+                      placeholder={placeholder}
+                      autoComplete="off"
+                      type={type}
+                      name={name}
+                      value={values[name]}
+                      className="border-b-2 border-white/20 w-full outline-none bg-black text-white focus:border-b-2 focus:border-white p-1.5"
+                      required
+                    />
+                    {touched && (
+                      <p className="text-sm mt-1 text-red-600 font-light">{errors[name]}</p>
+                    )}
+                  </div>
                 ))}
 
                 <p className="hidden">{values.role_id = role.id}</p>
+                <p className="hidden">{values.mobile_phone = values.mobile_phone.toString()}</p>
 
                 <div onClick={() => setOpenModalRole(!openModalRole)} className="hover:bg-white/30 flex justify-between items-center py-1.5 px-3 bg-white/20 cursor-pointer">
-                  <h1 className="text-sm font-bold text-green-600 drop-shadow-md">Role - {role.name}</h1>
+                  <h1 className="text-sm font-bold text-white drop-shadow-md">Role - {role.name}</h1>
                   <EditIcon className='w-4 aspect-square' />
                 </div>
 
@@ -86,11 +93,11 @@ const RegisterComponents: React.FC = () => {
               <div className='mt-12'>
 
                 {isSubmitting ? (
-                  <button disabled className='font-bold py-2 px-4 bg-green-800 hover:shadow-md hover:shadow-green-700/80 w-full'>
-                    ...
+                  <button disabled className='font-bold py-2 px-4 bg-green-800 hover:shadow-md hover:shadow-green-800/80 w-full'>
+                    Bentar...
                   </button>
                 ) : (
-                  <button type='submit' className='font-bold py-2 px-4 bg-green-600 hover:shadow-md hover:shadow-green-600/80 w-full'>
+                  <button disabled={Object.keys(errors).length !== 0} type='submit' className='font-bold py-2 px-4 bg-green-600 hover:shadow-md hover:shadow-green-600/80 w-full disabled:bg-green-800 disabled:text-white/60 disabled:shadow-green-800'>
                     Register
                   </button>
                 )}
@@ -119,6 +126,7 @@ const RegisterComponents: React.FC = () => {
 
         </>
       )}
+
     </>
   )
 }
---
+++ lib/utils/validation.ts
@@ -0,0 +1,36 @@
+// Lib
+import validator from 'validator'
+
+// Interface 
+import UserData from '../interface/UserData'
+
+type ErrorsMesaage = { [field: string]: string }
+
+// utils
+const max = (str: string, max: number) => str.length <= max
+const min = (str: string, min: number) => str.length >= min
+
+// Main Validator
+export const validateRegisterForm = (values: UserData) => {
+
+  const errors: ErrorsMesaage = {}
+  const { name, email, mobile_phone, password, address, password_confirmation } = values
+
+  if (!validator.isAlpha(name)) errors.name = "Nama tidak boleh mengandung spasi, symbol, atau angka"
+  if (!min(name, 4)) errors.name = "Nama harus diisi minimal 4 huruf"
+  if (!max(name, 20)) errors.name = "Nama harus diisi maksimal 20 huruf"
+
+  if (!validator.isEmail(email)) errors.email = "Email harus mengandung @ dan domain yang sesuai"
+
+  if (!validator.isMobilePhone(mobile_phone.toString(), 'id-ID')) errors.mobile_phone = "No HP tidak sesuai negara +62 hehe"
+  if (!min(mobile_phone.toString(), 11)) errors.mobile_phone = "No HP harus diisi minimal 11 digit"
+  if (!max(mobile_phone.toString(), 15)) errors.mobile_phone = "No HP harus diisi maksimal 15 digit"
+
+  if(!address) errors.address = "Alamat harus diisi"
+
+  if (!min(password, 8)) errors.password = "Password harus diisi minimal 8 karakter"
+  if (password_confirmation !== password) errors.password_confirmation = "Password berbeda"
+
+  return errors
+
+}
